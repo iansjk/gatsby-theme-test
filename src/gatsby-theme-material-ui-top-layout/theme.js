3d7c0ad78d1dabd14a9fd8182843bbc6e0c4077a
Ian Kim <iansjk@gmail.com>
2020-12-26 11:13:44 -0500

    Switch to dark theme test

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -4,6 +4,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
+    type: "dark",
     primary: {
       main: '#556cd6',
     },
@@ -13,9 +14,9 @@ const theme = createMuiTheme({
     error: {
       main: red.A400,
     },
-    background: {
-      default: '#fff',
-    },
+    // background: {
+    //   default: '#fff',
+    // },
   },
 });
 
